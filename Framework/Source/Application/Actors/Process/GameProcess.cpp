01fd3c9ae8dbb33de4f1d5250f124527708516f2
SyunsukeNodutsu <66367386+SyunsukeNodutsu@users.noreply.github.com>
2022-02-06 04:26:34 +0900

    BGMファイルのリネーム

--- Framework/Source/Application/Actors/Process/GameProcess.cpp
+++ Framework/Source/Application/Actors/Process/GameProcess.cpp
@@ -14,7 +14,7 @@ GameProcess::GameProcess()
 void GameProcess::Initialize()
 {
 	// BGM再生
-	auto sound = SOUND_DIRECTOR.CreateSoundWork("Resource/Audio/BGM/魔王魂  民族28.wav", true, true);
+	auto sound = SOUND_DIRECTOR.CreateSoundWork("Resource/Audio/BGM/minzoku28_maoudamasii.wav", true, true);
 	if (sound) {
 		sound->Play(1000);
 		sound->SetVolume(0.36f);
